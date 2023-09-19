4f290939c29ef18dbbc84cb79504101531b7a227
Andrey Dunaev <dunayev@yandex.ru>
2023-09-19 11:59:28 +0700

    feat настроить адаптивность для header

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header';
+import useResize from '../../customHooks/useResize';
 
 function App() {
+  const windowWidth = useResize(100);
   return (
     <div className="page root">
-      <Header />
+      <Header
+        place="main"
+        width={windowWidth}
+      />
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,26 +2,31 @@ import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 import Logo from '../Logo/Logo';
 import AcountButton from '../account/AccountButton';
+import BugrerButton from '../BurgerButton/BurgerButton';
 
-export default function Header() {
+export default function Header({ width, place }) {
   const loggedIn = false;
   return (
-    <header className={`header ${loggedIn ? 'header_loggedIn' : ''}`}>
+    <header
+      className={`header ${
+        loggedIn ? 'header_loggedIn' : ''
+      } header_place_${place}`}
+    >
       <Logo place="header" />
       <NavLink
         className={({ isActive }) =>
-          `header__link ${isActive ? 'header__link_active' : ''} ${
-            loggedIn ? 'header__link_visible' : ''
-          }`
+          `header__link header__nav-link ${
+            isActive ? 'header__link_active' : ''
+          } ${loggedIn ? 'header__link_visible' : ''}`
         }
       >
         Фильмы
       </NavLink>
       <NavLink
         className={({ isActive }) =>
-          `header__link ${isActive ? 'header__link_active' : ''} ${
-            loggedIn ? 'header__link_visible' : ''
-          }`
+          `header__link header__nav-link ${
+            isActive ? 'header__link_active' : ''
+          } ${loggedIn ? 'header__link_visible' : ''}`
         }
       >
         Сохраненные фильмы
@@ -40,7 +45,12 @@ export default function Header() {
       >
         Войти
       </Link>
-      {loggedIn ? <AcountButton></AcountButton> : ''}
+      {loggedIn && width > 768 ? <AcountButton></AcountButton> : ''}
+      {loggedIn && width < 768 ? (
+        <BugrerButton place="header"></BugrerButton>
+      ) : (
+        ''
+      )}
     </header>
   );
 }
