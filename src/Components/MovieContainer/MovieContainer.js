da0a1976341efa8e7e09bcf35766d8030e47df49
Andrey Dunaev <dunayev@yandex.ru>
2023-10-07 11:40:09 +0700

    ref вынести логику фильтрации, нарезки массиво фильмов на количество, кнопку еще в компонент movies , в movies-container оставить только ul

--- src/Components/MovieContainer/MovieContainer.js
+++ src/Components/MovieContainer/MovieContainer.js
@@ -1,80 +1,74 @@
 import './MovieContainer.css';
-import SectionTemplate from '../SectionTemplate/SectionTemplate';
 import MovieCard from '../MovieCard/MovieCard';
-import FilmSearchForm from '../FilmSearchForm/FilmSearchForm';
-import { useEffect, useState } from 'react';
-import renderMoviesSettings from '../../variables/renderMoviesSettings';
 
-export default function MovieContainer({ initialArray, width }) {
+export default function MovieContainer({ moviesArrayforMaping, amountTotal }) {
   // setStateOfPage({ ...{}, request: filmRequest, isShort: isShortFilm });
 
-  const [filmRequest, setFilmRequest] = useState('');
-  const [isShortFilm, setIsShortFilm] = useState(false);
-  // const [searchResalt, setSearchResult] = useState([]);
-  const [settingsForRender, setSettingsForRender] = useState({});
-  const [amountTotal, setAmountTotal] = useState(0);
-  const [moviesArrayforMaping, setMoviesArrayforMaping] = useState([]);
-  const [stateOfPage, setStateOfPage] = useState({});
-  console.log(document.querySelectorAll('.card'));
-  useEffect(() => {
-    const { filmRequest, isShortFilm, moviesArrayforMaping, amountTotal } =
-      JSON.parse(localStorage.getItem('moviesPage'));
-    if (moviesArrayforMaping) {
-      setFilmRequest(filmRequest);
-      setIsShortFilm(isShortFilm);
-      setMoviesArrayforMaping(moviesArrayforMaping);
-      setAmountTotal(amountTotal);
-    }
-  }, []);
+  // const [filmRequest, setFilmRequest] = useState('');
+  // const [isShortFilm, setIsShortFilm] = useState(false);
+  // const [settingsForRender, setSettingsForRender] = useState({});
+  // const [amountTotal, setAmountTotal] = useState(0);
+  // const [moviesArrayforMaping, setMoviesArrayforMaping] = useState([]);
+  // const [stateOfPage, setStateOfPage] = useState({});
+  // useEffect(() => {
+  //   const { filmRequest, isShortFilm, moviesArrayforMaping, amountTotal } =
+  //     JSON.parse(localStorage.getItem('moviesPage'));
+  //   if (moviesArrayforMaping) {
+  //     setFilmRequest(filmRequest);
+  //     setIsShortFilm(isShortFilm);
+  //     setMoviesArrayforMaping(moviesArrayforMaping);
+  //     setAmountTotal(amountTotal);
+  //   }
+  // }, []);
 
-  // эффект контроля ширины экрана назначает набор переменных для отрисовки
-  useEffect(() => {
-    setSettingsForRender(
-      width > 768
-        ? renderMoviesSettings.bigScreen
-        : width > 520
-        ? renderMoviesSettings.mediumScreen
-        : renderMoviesSettings.smallScreen
-    );
-  }, [width]);
+  // // эффект контроля ширины экрана назначает набор переменных для отрисовки
+  // useEffect(() => {
+  //   setSettingsForRender(
+  //     width > 768
+  //       ? renderMoviesSettings.bigScreen
+  //       : width > 520
+  //       ? renderMoviesSettings.mediumScreen
+  //       : renderMoviesSettings.smallScreen
+  //   );
+  // }, [width]);
 
-  // эффект устанавливает первоначальное количество карточек
-  useEffect(() => {
-    setAmountTotal(settingsForRender.amountInit);
-  }, [settingsForRender.amountInit]);
+  // // эффект устанавливает первоначальное количество карточек
+  // useEffect(() => {
+  //   setAmountTotal(settingsForRender.amountInit);
+  // }, [settingsForRender.amountInit]);
 
-  // эффект записывает состояние страницы в переменнную
-  useEffect(() => {
-    setStateOfPage({
-      filmRequest,
-      isShortFilm,
-      moviesArrayforMaping,
-      amountTotal,
-    });
-  }, [filmRequest, isShortFilm, moviesArrayforMaping, amountTotal]);
+  // // эффект записывает состояние страницы в переменнную
+  // useEffect(() => {
+  //   setStateOfPage({
+  //     filmRequest,
+  //     isShortFilm,
+  //     moviesArrayforMaping,
+  //     amountTotal,
+  //   });
+  // }, [filmRequest, isShortFilm, moviesArrayforMaping, amountTotal]);
 
-  // эффект записывает состояние страницы в localstorage
-  useEffect(() => {
-    console.log('stateOfPage');
-    localStorage.setItem('moviesPage', JSON.stringify(stateOfPage));
-  }, [stateOfPage]);
+  // // эффект записывает состояние страницы в localstorage
+  // useEffect(() => {
+  //   console.log('stateOfPage');
+  //   localStorage.setItem('moviesPage', JSON.stringify(stateOfPage));
+  // }, [stateOfPage]);
 
-  const handleButton = () => {
-    setAmountTotal(amountTotal + settingsForRender.amountForAdd);
-  };
-  const submitForm = () => {
-    setMoviesArrayforMaping(
-      initialArray.filter((movie) =>
-        filmRequest === ''
-          ? isShortFilm
-            ? movie.duration <= 40
-            : movie.duration
-          : (movie.nameRU.toLowerCase().includes(filmRequest.toLowerCase()) ||
-              movie.nameEN.toLowerCase().includes(filmRequest.toLowerCase())) &&
-            (isShortFilm ? movie.duration <= 40 : movie.duration)
-      )
-    );
-  };
+  // const handleButton = () => {
+  //   setAmountTotal(amountTotal + settingsForRender.amountForAdd);
+  // };
+  // const submitForm = () => {
+  //   setMoviesArrayforMaping(
+  //     initialArray.filter((movie) =>
+  //       filmRequest === ''
+  //         ? isShortFilm
+  //           ? movie.duration <= 40
+  //           : movie.duration
+  //         : (movie.nameRU.toLowerCase().includes(filmRequest.toLowerCase()) ||
+  //             movie.nameEN.toLowerCase().includes(filmRequest.toLowerCase())) &&
+  //           (isShortFilm ? movie.duration <= 40 : movie.duration)
+  //     )
+  //   );
+  // };
   const movies = moviesArrayforMaping.slice(0, amountTotal).map((movie) => {
     return (
       <li
@@ -92,33 +86,5 @@ export default function MovieContainer({ initialArray, width }) {
     );
   });
 
-  return (
-    <div className="movie-container">
-      <SectionTemplate place="movie-container">
-        <FilmSearchForm
-          submit={submitForm}
-          checkboxState={isShortFilm}
-          checkboxSetter={setIsShortFilm}
-          onChange={setFilmRequest}
-          value={filmRequest}
-          width={width}
-          place="movie-container"
-        />
-
-        <ul className="movie-container__list">{movies}</ul>
-
-        <button
-          onClick={handleButton}
-          type="button"
-          className={`movie-container__button ${
-            amountTotal < moviesArrayforMaping.length
-              ? 'movie-container__button_visible'
-              : ''
-          }`}
-        >
-          Еще
-        </button>
-      </SectionTemplate>
-    </div>
-  );
+  return <ul className="movie-container">{movies}</ul>;
 }
--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -1,31 +1,104 @@
 import './Movies.css';
 import MovieContainer from '../MovieContainer/MovieContainer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import SectionTemplate from '../SectionTemplate/SectionTemplate';
+import FilmSearchForm from '../FilmSearchForm/FilmSearchForm';
+import renderMoviesSettings from '../../variables/renderMoviesSettings';
 
 export default function Movies({ width, movies }) {
-  //   const [filmRequest, setFilmRequest] = useState('');
-  //   const [isShortFilm, setIsShortFilm] = useState(false);
-  //   const [searchResalt, setSearchResalt] = useState([]);
-  //   const [stateOfPage, setStateOfPage] = useState({});
-  //   const submitForm = () => {
-  //     setSearchResalt(
-  //       movies.filter((movie) =>
-  //         filmRequest === ''
-  //           ? isShortFilm
-  //             ? movie.duration <= 40
-  //             : movie.duration
-  //           : (movie.nameRU.toLowerCase().includes(filmRequest.toLowerCase()) ||
-  //               movie.nameEN.toLowerCase().includes(filmRequest.toLowerCase())) &&
-  //             (isShortFilm ? movie.duration <= 40 : movie.duration)
-  //       )
-  //     );
-  //   };
+  const [filmRequest, setFilmRequest] = useState('');
+  const [isShortFilm, setIsShortFilm] = useState(false);
+  const [settingsForRender, setSettingsForRender] = useState({});
+  const [amountTotal, setAmountTotal] = useState(0);
+  const [moviesArrayforMaping, setMoviesArrayforMaping] = useState([]);
+  const [stateOfPage, setStateOfPage] = useState({});
+  useEffect(() => {
+    const { filmRequest, isShortFilm, moviesArrayforMaping, amountTotal } =
+      JSON.parse(localStorage.getItem('moviesPage'));
+    if (moviesArrayforMaping) {
+      setFilmRequest(filmRequest);
+      setIsShortFilm(isShortFilm);
+      setMoviesArrayforMaping(moviesArrayforMaping);
+      setAmountTotal(amountTotal);
+    }
+  }, []);
+
+  // эффект контроля ширины экрана назначает набор переменных для отрисовки
+  useEffect(() => {
+    setSettingsForRender(
+      width > 768
+        ? renderMoviesSettings.bigScreen
+        : width > 520
+        ? renderMoviesSettings.mediumScreen
+        : renderMoviesSettings.smallScreen
+    );
+  }, [width]);
+
+  // эффект устанавливает первоначальное количество карточек
+  useEffect(() => {
+    setAmountTotal(settingsForRender.amountInit);
+  }, [settingsForRender.amountInit]);
+
+  // эффект записывает состояние страницы в переменнную
+  useEffect(() => {
+    setStateOfPage({
+      filmRequest,
+      isShortFilm,
+      moviesArrayforMaping,
+      amountTotal,
+    });
+  }, [filmRequest, isShortFilm, moviesArrayforMaping, amountTotal]);
+
+  // эффект записывает состояние страницы в localstorage
+  useEffect(() => {
+    console.log('stateOfPage');
+    localStorage.setItem('moviesPage', JSON.stringify(stateOfPage));
+  }, [stateOfPage]);
+
+  const submitForm = () => {
+    setMoviesArrayforMaping(
+      movies.filter((movie) =>
+        filmRequest === ''
+          ? isShortFilm
+            ? movie.duration <= 40
+            : movie.duration
+          : (movie.nameRU.toLowerCase().includes(filmRequest.toLowerCase()) ||
+              movie.nameEN.toLowerCase().includes(filmRequest.toLowerCase())) &&
+            (isShortFilm ? movie.duration <= 40 : movie.duration)
+      )
+    );
+  };
+  const handleButton = () => {
+    setAmountTotal(amountTotal + settingsForRender.amountForAdd);
+  };
   return (
-    <main className="moives">
-      <MovieContainer
-        width={width}
-        initialArray={movies}
-      ></MovieContainer>
+    <main className="movies">
+      <SectionTemplate place="movies">
+        <FilmSearchForm
+          submit={submitForm}
+          checkboxState={isShortFilm}
+          checkboxSetter={setIsShortFilm}
+          onChange={setFilmRequest}
+          value={filmRequest}
+          width={width}
+          place="movie-container"
+        />
+        <MovieContainer
+          moviesArrayforMaping={moviesArrayforMaping}
+          amountTotal={amountTotal}
+        ></MovieContainer>
+        <button
+          onClick={handleButton}
+          type="button"
+          className={`movies__button ${
+            amountTotal < moviesArrayforMaping.length
+              ? 'movies__button_visible'
+              : ''
+          }`}
+        >
+          Еще
+        </button>
+      </SectionTemplate>
     </main>
   );
 }
